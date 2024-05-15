802ae389f5d06588d872189360099405f2308409
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-05-15 13:29:19 +0300

    add

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,14 @@
-import Contact from "../Contact/Contact";
-// import css from "../ContactList/ContactList.module.css";
+// import Contact from "../Contact/Contact";
+// // import css from "../ContactList/ContactList.module.css";
 
-export default function ContactList({ contacts, onDelete }) {
-  return (
-    <ul>
-      {contacts.map((contact) => (
-        <li key={contact.id}>
-          <Contact data={contact} onDelete={onDelete} />
-        </li>
-      ))}
-    </ul>
-  );
-}
+// export default function ContactList({ contacts, onDelete }) {
+//   return (
+//     <ul>
+//       {contacts.map((contact) => (
+//         <li key={contact.id}>
+//           <Contact contacts={contact} onDelete={onDelete} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
